e17b45e8e53b309678e08198e9e0115ce87ece99
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-21 17:38:46 -0700

    Added <h1>, <h2>, <h3>, and <p> tags to the milestone-1.php file.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,32 @@
 		<title>Milestone 1 Documentation</title>
 	</head>
 	<body>
-
+		<h1>Personal Website Project - Milestone 1</h1>
+			<h2>Purpose Audience, and Goals</h2>
+				<h3>Purpose</h3>
+					<p>This project is a website to promote my services as an ESL instructor.</p>
+				<h3>Audience</h3>
+					<p>The site is intended for non-native English speakers who are interested in learning the language or improving their skills. Learners will either be based in Albuquerque for in-person instruction or outside of the area for online lessons on Verbling.com (online tutoring for Albuquerque residents will also be available, of course). The target audience will be comprised mostly of English learners and potentially language schools that are interested in recruiting part-time teachers.</p>
+				<h3>Goals</h3>
+					<p>The primary goal is to gain new English as a Second Language (ESL) students. The second goal is to connect with online or local schools that are looking for new instructors.</p>
+			<h2>Persona</h2>
+				<h3>Name</h3>
+					<p>Moniz Mohammed</p>
+				<h3>Age</h3>
+					<p>35 years old</p>
+				<h3>Location</h3>
+					<p>Houston, Texas</p>
+				<h3>Occupation</h3>
+					<p>Moniz is a manager in the Risk Management department at Capco Energy Solutions, a global consultancy firm with an office in Houston, Texas. At Capco, Moniz focuses on deal capture systems, pricing systems, product support, and risk analytics and modeling.</p>
+				<h3>Technology</h3>
+					<p>As Moniz has a Master's degree in Computer Applications, he is very comfortable with technology. At work he uses a 27-inch Dell Precision 5720 All-in-One desktop that runs on Windows 10 Pro. His personal smart phone is an iPhone 7 on which iOS 12 is installed. Additionally, last month he purchased a 13-inch MacBook Pro and installed macOS Mojave.</p>
+				<h3>Attitudes and Behaviors</h3>
+					<p>Moniz has a high-intermediate to lower-advanced level in English and wants to improve. As he has two small children at home, he needs to be at home in the evening and thus prefers online tutoring. Before he chooses to book a lesson with an instructor, he likes to find out as much as possible about their teaching style and their personality to make sure he has found a good match.</p>
+				<h3>Frustrations and needs</h3>
+					<p>Moniz is fairly comfortable with English, as evidenced by his managerial position. However, he is aware that he still makes grammatical errors and has to rely on prepared notes when presenting at meetings. He wants to begin taking regular lessons to finally master the language, but he is too busy with work and familial obligations to attend night classes. The idea of online tutoring appeals to him, but he wants to be sure his teacher will be both professional and personable, so he prefer more information than a one-paragraph bio. As a programmer himself, he respects an instructor takes the time to put together a professional website.</p>
+				<h3>Goals</h3>
+					<p>In six months, Moniz hopes to see a significant improve in his speaking and writing abilities. Additionally, he is considering going back to school part-time in a year or two to get his MBA, so he would need to prepare for and take the Test of English as a Foreign Language (TOEFL). He needs a teacher who is willing to adapt to his needs while providing fun and engaging instruction, so he hopes to quickly find an online tutor during his limited free time.</p>
+				<h3>User Story</h3>
+					<p>Moniz Mohammed is looking to find an online ESL tutor who can help him master English.</p>
 	</body>
 </html>
