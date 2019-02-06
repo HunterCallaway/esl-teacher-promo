eb24194dc2b243ae8e6f6fb608581a71631ce25a
Hunter Callaway <HunterCallaway@hotmail.com>
2019-02-06 09:39:03 -0700

    Created a php file called milestone-2.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>Milestone 1</h1>
+			<h2>Content Strategy</h2>
+	</body>
+</html>
