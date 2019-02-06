17e8d082c9fe0a5630f0d91f8508aea6bdd2c019
Hunter Callaway <HunterCallaway@hotmail.com>
2019-02-06 10:41:31 -0700

    Added the Content Strategy.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,5 +7,14 @@
 	<body>
 		<h1>Milestone 1</h1>
 			<h2>Content Strategy</h2>
+					<p>My one-page personal website will be composed of the following sections:</p>
+				<h3>Welcome</h3>
+					<p>This section will prominently feature the name of the website, "Learn English with Hunter". The name will be displayed over a picture of a desk on which grammar books, a notebook, an open laptop, and writing utensils have been arranged.</p>
+				<h3>About Me</h3>
+					<p>In this section, I will include a portrait-style photograph of myself along with an introductory paragraph. The text will explain that I am a certified English as a Second Language (ESL) instructor and that I am available for in-person lessons in Albuquerque and online. I want to present myself as professional yet personable, so students will understand they can expect a tutor who will have a detailed plan for each lesson, all while providing fun and engaging instruction. The image will slide in from the left and the text will slide in from the right as the page loads.</p>
+				<h3>Experience and Testimonial</h3>
+					<p>This part of the website will feature information about my time as an instructor on Verbling.com and my participation in the Teaching Assistant Program in France (TAPIF). I will explain that I have a Certificate in English Language Teaching to Adults (CELTA). Additionally, one of my former students will provide a short paragraph attesting to my abilities as an instructor. Each block of information and its associated logo/image, if any, (i.e. the "Experience" section title, Verbling teaching, TAPIF, CELTA information, the "Testimonial" sub-section title, and testimonial content) will slide in alternatingly from the left and the right as the user scrolls  to the end. The image of the CELTA will also expanded to an enlarged PDF upon clicking it.</p>
+				<h3>Contact</h3>
+					<p>I will create a contact form that will allow potential students (and local schools who may be looking for part-time instructors) to get in touch with me for further information. There will also be a link to my Verbling.com teacher profile and my LinkedIn and GitHub accounts.</p>
 	</body>
 </html>
