febca0c89e40ce7a4b1d4ed2550e657f6f8b06e1
Hunter Callaway <HunterCallaway@hotmail.com>
2019-02-14 13:36:39 -0700

    Started implementing the html.

---
+++ public_html/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+		<Title>Learn English with Hunter</Title>
+	</head>
+	<body>
+
+		<header>
+
+		</header>
+
+		<section>
+
+		</section>
+
+		<section>
+
+		</section>
+
+		<section>
+
+		</section>
+
+		<footer>
+
+		</footer>
+
+	</body>
+</html>
